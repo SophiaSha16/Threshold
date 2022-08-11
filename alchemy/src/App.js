62881bf2d597088810a33f07b38d9c0968a9f6d7
sophiaMonkeytech <95685230+sophiaMonkeytech@users.noreply.github.com>
2022-08-11 13:17:26 +0300

    Basic css

--- alchemy/src/App.js
+++ alchemy/src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='mainCon'>
+
+        <div className="generatorCon">
+          <div className="generator"></div>
+          <div className="generatorOutput"></div>
+        </div>
+
+        <div className="styleCol"></div>
+
+        <div className="comments"></div>
+      </div>
     </div>
   );
 }
